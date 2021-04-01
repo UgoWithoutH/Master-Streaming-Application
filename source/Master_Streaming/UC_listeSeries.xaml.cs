652a06515d57481863912e90115d2b52cb22b855
ugvignon <ugvignon@3f70320b-3022-4607-9211-072dac9809af>
2021-04-01 18:40:56 +0000

    Début de la vue en XAML(header et listes de séries) -> MainWindow avec des User Control -> Il manque la barre de recherche ainsi que les boutons de filtrage des listes

---
+++ source/Master_Streaming/UC_listeSeries.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Master_Streaming
+{
+    /// <summary>
+    /// Logique d'interaction pour UC_listeSeries.xaml
+    /// </summary>
+    public partial class UC_listeSeries : UserControl
+    {
+        public UC_listeSeries()
+        {
+            InitializeComponent();
+        }
+    }
+}
