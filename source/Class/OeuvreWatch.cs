f3cc53f6265cf70e37a57feafe6e7d323390e945
machaumont2 <machaumont2@3f70320b-3022-4607-9211-072dac9809af>
2021-05-01 17:46:46 +0000

    ajout des fichier (j'ai pas add au dernier commit)

---
+++ source/Class/OeuvreWatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    class OeuvreWatch : Oeuvre
+    {
+        public DateTime TimeAdd { get; }
+        public Oeuvre OriginalOeuvre { get; }
+
+        public OeuvreWatch (DateTime timeAdd, Oeuvre o)
+            :base(o.Titre, o.DateSortie, o.Description, o.Note, o.ImageName, o.TagsGenres)
+        {
+            TimeAdd = timeAdd;
+            OriginalOeuvre = o;
+        }
+
+        public OeuvreWatch (DateTime timeAdd, Oeuvre o, string titre, DateTime dateSortie, string description, int? note, string imageName, HashSet<Genre> tagsgenres)
+            :base (titre, dateSortie, description, note, imageName, tagsgenres)
+        {
+            TimeAdd = timeAdd;
+            OriginalOeuvre = o;
+        }
+
+        public OeuvreWatch(DateTime timeAdd, Oeuvre o, int? note, List<Auteur> listAuteurs, string titre, DateTime dateSortie, string description, string imageName, HashSet<Genre> tagsgenres)
+           : base(titre, dateSortie, description, note, imageName, listAuteurs, tagsgenres)
+        {
+            TimeAdd = timeAdd;
+            OriginalOeuvre = o;
+        }
+    }
+}
---
+++ source/Test_Watchlist/Program.cs
@@ -0,0 +1,19 @@
+﻿using Class;
+using System;
+using System.Collections.Generic;
+
+namespace TestWatchlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Oeuvre s1 = new Serie("Elite", DateTime.Now, "C'est cool", 4, "///////", 52, new List<Auteur>() { new Auteur("Jean","Paul",Métier.Acteur),
+                                                                                                             new Auteur("Paul","Jack",Métier.Cascadeur)}, new HashSet<Genre>() { new Genre("Drame") });
+
+            Watchlist w = new Watchlist();
+            Console.WriteLine($"La valeur de retour de l'ajout est { w.AjouterOeuvre(s1) } ");
+            Console.WriteLine($"La valeur de retour de la suppression est { w.SupprimerOeuvre(s1) } ");
+        }
+    }
+}
