07f80902d15b987d60eb0348a4a2030feb2b9a50
ugvignon <ugvignon@3f70320b-3022-4607-9211-072dac9809af>
2021-05-05 12:50:06 +0000

    Ajout de ce que j'ai oublié

---
+++ trunk/source/TestU_Recherche/UnitTest1.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class;
+using System.Collections.Generic;
+using System;
+
+namespace TestU_Recherche
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRecherche()
+        {
+            var ensemble = new Dictionary<Genre, List<Oeuvre>>();
+
+            var l1 = new List<Oeuvre>()
+            {
+                new Serie("Elite",new DateTime(2019,10,1),"Série mêlant Drame et Amour",0,"////",3, new HashSet<Genre>(){new Genre("Drame"), new Genre("Amour")}),
+                new Serie("La casa de papel",DateTime.Now,"Série mêlant Drame et Action",null,"////",3, new HashSet<Genre>(){new Genre("Drame")}),
+                new Serie("La petite maison dans la prairie",new DateTime(2000,02,20),"Pas vraiement une série",null,"////",0, new HashSet<Genre>(){new Genre("Drame")}),
+            };
+
+            var l2 = new List<Oeuvre>()
+            {
+                new Serie("Elite",new DateTime(2019,10,1),"Série mêlant Drame et Amour",null,"////",3, new HashSet<Genre>(){new Genre("Drame"), new Genre("Amour")}),
+                new Serie("Une série",DateTime.Now,"Série mêlant Amour",null,"////",3, new HashSet<Genre>(){new Genre("Amour")}),
+                new Serie("Bonne une série",new DateTime(2000,02,20),"Pas vraiment une série",null,"////",0, new HashSet<Genre>(){new Genre("Amour")}),
+            };
+
+            ensemble.Add(new Genre("Drame"), l1);
+            ensemble.Add(new Genre("Amour"), l2);
+
+           
+            Assert.AreEqual(ensemble.Count, 2);
+
+            foreach (var listing in ensemble)
+            {
+                Assert.AreEqual(listing.Value.Count, 3);
+            }
+
+            var MaRecherche = ensemble.RechercherOeuvres("elite");
+            Assert.AreEqual(MaRecherche.Count, 1);
+            foreach (Oeuvre o in MaRecherche)
+            {
+                Assert.IsTrue(o.Titre.StartsWith("Elite"));
+            }
+        }
+    }
+}
---
+++ trunk/source/Test_ListDates/Program.cs
@@ -0,0 +1,23 @@
+﻿using Class;
+using System;
+using System.Collections.Generic;
+
+namespace Test_ListDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new SortedSet<int>();
+
+            List.Add(50);
+            List.Add(20);
+            List.Add(110);
+
+            foreach (int i in List)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
