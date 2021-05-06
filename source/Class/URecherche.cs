2fc3fdcf3c877b99723f2ea129012ec40c14ab25
ugvignon <ugvignon@3f70320b-3022-4607-9211-072dac9809af>
2021-05-06 07:26:38 +0000

    classe abstraite ObservableObject

--- source/Class/URecherche.cs
+++ source/Class/URecherche.cs
@@ -12,7 +12,7 @@ namespace Class
         /// <param name="données">contient l'ensemble des listes associées à leurs genres</param>
         /// <param name="chaine">chaine de caractères devant être comparée au début du Titre des Oeuvres</param>
         /// <returns>La liste des Oeuvres contenant au début de leur Titre la chaine de caractères</returns>
-        public static List<Oeuvre> RechercherOeuvres(this Dictionary<Genre, List<Oeuvre>> données, string chaine) //this pour lier la méthode aux Dictionary
+        public static List<Oeuvre> RechercherOeuvres(this Dictionary<Genre, List<Oeuvre>> données, string chaine) //this pour faire une méthode d'extension
         {
             var résultat = new List<Oeuvre>();
             List<Oeuvre> listVariable = null;
