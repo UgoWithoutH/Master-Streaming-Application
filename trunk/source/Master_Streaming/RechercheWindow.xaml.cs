690ff108c562a50749bf1e680cebb54e5326690f
machaumont2 <machaumont2@3f70320b-3022-4607-9211-072dac9809af>
2021-05-07 19:45:09 +0000

    ajout fichiers oubliés d'add au dernier commit

---
+++ trunk/source/Master_Streaming/RechercheWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Master_Streaming
+{
+    /// <summary>
+    /// Logique d'interaction pour RechercheWindow.xaml
+    /// </summary>
+    public partial class RechercheWindow : Window
+    {
+        public RechercheWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_To_Menu_Clicked (object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
