ac1b9743d6c124659a2fc5b08ab48dd89f1929c2
machaumont2 <machaumont2@3f70320b-3022-4607-9211-072dac9809af>
2021-05-07 19:05:38 +0000

    ajout de différentes fenêtres, le Binding de la Watchlist restera à faire

--- trunk/source/Master_Streaming/UC_header.xaml.cs
+++ trunk/source/Master_Streaming/UC_header.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Class;
 
 namespace Master_Streaming
 {
@@ -18,14 +19,35 @@ namespace Master_Streaming
     /// </summary>
     public partial class UC_header : UserControl
     {
+        public ProfilManager Manager => (App.Current as App).Pmanager;
+
         public UC_header()
         {
             InitializeComponent();
+            DataContext = Manager;
         }
 
         private void ButtonPopUpLogout_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void Ajouter_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            var AddWindow = new AjouterWindow();
+            AddWindow.Show();
+        }
+
+        private void Watchlist_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            var theWatchlist = new WatchlistWindow();
+            theWatchlist.Show();
+        }
+
+        private void Recherche_Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            var theRecherche = new RechercheWindow();
+            theRecherche.Show();
         }
     }
 }
