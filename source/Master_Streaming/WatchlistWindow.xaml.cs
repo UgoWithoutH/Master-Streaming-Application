4464e8a8f2ba565d92998c10f4a0cca7f7e1e281
machaumont2 <machaumont2@3f70320b-3022-4607-9211-072dac9809af>
2021-05-07 19:45:09 +0000

    ajout fichiers oubliés d'add au dernier commit

---
+++ source/Master_Streaming/WatchlistWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Class;
+
+namespace Master_Streaming
+{
+    /// <summary>
+    /// Logique d'interaction pour WatchlistWindow.xaml
+    /// </summary>
+    public partial class WatchlistWindow : Window
+    {
+        public ProfilManager Manager => (App.Current as App).Pmanager;
+
+        public WatchlistWindow()
+        {
+            InitializeComponent();
+            DataContext = Manager;
+        }
+
+        private void Back_To_Menu_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
