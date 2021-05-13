19394af0a1fbc62cfa3aedf14109d799629a360f
machaumont2 <machaumont2@3f70320b-3022-4607-9211-072dac9809af>
2021-05-13 19:12:53 +0000

    add des fichiers oubliés commit précédent

---
+++ trunk/source/Class/MainManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Class
+{
+    public class MainManager
+    {
+        public ProfilManager ProfilCourant { get; set; }
+
+        public ObservableCollection<ProfilManager> ListProfils { get; private set; }
+
+        public MainManager()
+        {
+            ProfilCourant = null;
+            ListProfils = new ObservableCollection<ProfilManager>();
+        }
+
+        public bool AjouteProfil(string nom)
+        {
+            if (!nom.Equals(""))
+            {
+                ListProfils.Add(new ProfilManager(nom));
+                return true;
+            }
+            return false;
+        }
+
+        public bool SupprimeProfil(string nom)
+        {
+            return ListProfils.Remove(new ProfilManager(nom));
+        }
+
+        public bool Connexion(string nom)
+        {
+            foreach (ProfilManager pm in ListProfils)
+                if (pm.CompareTo(new ProfilManager(nom)) == 0) ///truc magique jsp si ça marche
+                {
+                    ProfilCourant = pm;
+                    return true;
+                }
+            return false;
+        }
+
+        public void Deconnexion(string nom)
+        {
+            ProfilCourant = null;
+        }
+    }
+}
