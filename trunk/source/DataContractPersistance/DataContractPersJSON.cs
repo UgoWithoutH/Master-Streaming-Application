324b83816eef73bec28e1a7e0372565cc6d8aef3
ugvignon <ugvignon@3f70320b-3022-4607-9211-072dac9809af>
2021-06-02 08:15:03 +0000

    ajout DataContractPersJSON

---
+++ trunk/source/DataContractPersistance/DataContractPersJSON.cs
@@ -0,0 +1,35 @@
+﻿using Class;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Xml;
+
+namespace DataContractPersistance
+{
+    public class DataContractPersJSON : DataContractPers
+    {
+        public DataContractPersJSON()
+        {
+            RelativePath = "../../../../Master_Streaming/bin/Debug/JSON";
+            FileName = "Master_Streaming.json";
+            Serializer = new DataContractJsonSerializer(typeof(ObservableCollection<ProfilManager>));
+        }
+
+        public override void SauvegardeDonnées(ObservableCollection<ProfilManager> ListProfils)
+        {
+            if (!Directory.Exists(FilePath))
+            {
+                Directory.CreateDirectory(FilePath);
+            }
+
+            using (Stream writer = File.Create(FileNameAndPath))
+            {
+                Serializer.WriteObject(writer, ListProfils);
+            }
+        }
+    }
+}
+
